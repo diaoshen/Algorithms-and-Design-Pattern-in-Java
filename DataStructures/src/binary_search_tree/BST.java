a7401c42bcdfa81bc471e1aa354f683105c825bf
DIAO <DIAO@DESKTOP-7829P01>
2018-12-15 23:57:24 -0800

    asd

--- DataStructures/src/binary_search_tree/BST.java
+++ DataStructures/src/binary_search_tree/BST.java
@@ -332,8 +332,8 @@ public class BST <Key extends Comparable<Key> , Value>{
 			 * If above is confusing. Take a look at example
 			 * 
 			 * Case 1:
-			 			S
-						S
+			 		S
+					S
 					   / \
 					  E   X
 					 / \
