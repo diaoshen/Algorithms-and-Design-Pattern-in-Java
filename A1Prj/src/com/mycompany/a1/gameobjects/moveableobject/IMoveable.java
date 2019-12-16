573203970d2520953922dc370a9beebcc95382fc
DIAOSHEN <isolvethis@gmail.com>
2019-12-15 17:11:05 -0800

    Cleaned up

--- A1Prj/src/com/mycompany/a1/gameobjects/moveableobject/IMoveable.java
+++
@@ -1,5 +0,0 @@
-package com.mycompany.a1.gameobjects.moveableobject;
-
-public interface IMoveable {
-	public void move();
-}
--- A1Prj/src/com/mycompany/a1/gameobjects/moveableobject/ISteerable.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.a1.gameobjects.moveableobject;
-
-public interface ISteerable {
-	public void turnLeft();
-	public void turnRight();
-}
